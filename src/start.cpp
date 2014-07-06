edd7d61b24fa8eeac374c780bdc9a2966134a93a
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-07-06 12:46:46 +0200

    Templat for TopCoder SRM match

---
+++ src/start.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+class AAA {
+public:
+
+};
+
+//int main() {
+//    /* Check that standard c++11 is enabled
+//     * 
+//     * If it doesn't compile then:
+//     * Project Settings -> C/C++ Build -> Settings -> Tool Settings -> GCC C++ Compiler ->
+//     * Miscellaneous -> Other Flags. Put -std=c++11 at the end
+//     */
+//    vector<int> numbers = { 1, 2, 3 };
+//    for (auto number: numbers) {
+//        cout << number << endl;
+//    }
+//}
